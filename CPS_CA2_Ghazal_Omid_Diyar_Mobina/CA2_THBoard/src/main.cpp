2ab7c9c83f4d62b0e5c3c4881f330611995ae260
Mobina <shbmobina@gmail.com>
2021-03-18 21:46:10 +0330

    Add TH-board hex file

--- CPS_CA2_Ghazal_Omid_Diyar_Mobina/CA2_THBoard/src/main.cpp
+++ CPS_CA2_Ghazal_Omid_Diyar_Mobina/CA2_THBoard/src/main.cpp
@@ -23,28 +23,30 @@ void loop()
 
 void getTemperatureFromSensor()
 {
+	unsigned int data[2];
 	Wire.beginTransmission(Addr);
 	Wire.write(0xF3);
 	Wire.endTransmission();
 	delay(500);
 	Wire.requestFrom(Addr, 2);
 	if (Wire.available() == 2) {
-    unsigned int lsb = Wire.read();
-		unsigned int msb = Wire.read();
-		temperature = (((lsb * 256.0 + msb) * 175.72) / 65536.0) - 46.85;
+    data[0] = Wire.read();
+		data[1] = Wire.read();
+		temperature = (((data[0] * 256.0 + data[1]) * 175.72) / 65536.0) - 46.85;
 	}
 }
 
 void getHumidityFromSensor()
 {
+	unsigned int data[2];
 	Wire.beginTransmission(Addr);
 	Wire.write(0xF5);
 	Wire.endTransmission();
 	delay(500);
 	Wire.requestFrom(Addr, 2);
 	if (Wire.available() == 2) {
-		unsigned int lsb = Wire.read();
-		unsigned int msb = Wire.read();
-		humidity = (((lsb * 256.0 + msb) * 125.0) / 65536.0) - 6;
+		data[0] = Wire.read();
+		data[1] = Wire.read();
+		humidity = (((data[0] * 256.0 + data[1]) * 125.0) / 65536.0) - 6;
 	}
 }
