2a02b1e9e862f5690d38fd292c8b8e17d220557d
Mobina <shbmobina@gmail.com>
2021-03-26 22:15:24 +0430

    Fix bug in showing fonts on real devices

--- CPS_CA3_Ghazal_Omid_Diyar_Mobina/Legend of Bounce/app/src/main/java/com/mobina/legendofbounca/activity/MainActivity.java
+++ CPS_CA3_Ghazal_Omid_Diyar_Mobina/Legend of Bounce/app/src/main/java/com/mobina/legendofbounca/activity/MainActivity.java
@@ -2,10 +2,12 @@ package com.mobina.legendofbounca.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.mobina.legendofbounca.R;
 import com.mobina.legendofbounca.core.config.GameConfig;
@@ -15,13 +17,19 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        TextView tx = findViewById(R.id.main_title);
+        Typeface customFont = Typeface.createFromAsset(getAssets(),
+            "whatever_it_takes.ttf");
+        tx.setTypeface(customFont);
+
         Button gyroscopeButton = findViewById(R.id.button_gyroscope);
         Button gravityButton = findViewById(R.id.button_gravity);
+        gyroscopeButton.setTypeface(customFont);
+        gravityButton.setTypeface(customFont);
 
         gyroscopeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
