93b30562739dc113d875b754bd4f88d4fb74dd8a
Mobina <shbmobina@gmail.com>
2021-05-31 23:36:33 +0430

    Merge dev and main branches

--- CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/MainActivity.java
+++ CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/MainActivity.java
@@ -7,20 +7,33 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mobina.cocoruncontroller.core.BluetoothService;
+import com.mobina.cocoruncontroller.core._3dVector;
+import com.mobina.cocoruncontroller.utils.GameConfig;
 
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
@@ -45,9 +58,15 @@ public class MainActivity extends Activity {
     private static final int REQUEST_CONNECT_DEVICE = 1;
     private static final int REQUEST_ENABLE_BT = 2;
 
-    ListView lv_paired_devices;
     Set<BluetoothDevice> pairedDevices;
     ArrayAdapter adapterPairedDevices;
+
+    private Button button;
+    private Vibrator vibrator;
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor, magnetometerSensor, gyroscopeSensor, gravitySensor, gameRotationSensor;
+    private double accLastEventTimestamp, magLastEventTimestamp, gyroscopeLastEventTimestamp, gravityLastEventTimestamp, gameRotationLastEventTimestamp;;
+    private _3dVector accelerometerTheta, magnetometerTheta, gyroscopeTheta, gravityTheta, gameRotationTheta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +89,42 @@ public class MainActivity extends Activity {
         }
       });
 
+      button = findViewById(R.id.button);
+      vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+      button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          if (Build.VERSION.SDK_INT >= 26) {
+            vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+          } else {
+            vibrator.vibrate(200);
+          }
+        }
+      });
+      accelerometerTheta = new _3dVector(0, 0, 0);
+      magnetometerTheta = new _3dVector(0, 0, 0);
+      gyroscopeTheta = new _3dVector(0, 0, 0);
+      gravityTheta = new _3dVector(0, 0, 0);
+      gameRotationTheta = new _3dVector(0, 0, 0);
+
+//      Timer timer = new Timer();
+//      timer.schedule(new TimerTask() {
+//        @Override
+//        public void run() {
+//          TextView tv = findViewById(R.id.textView);
+//          double steerAngle = Math.asin(gameRotationTheta.y) * 360;
+//
+//          if (steerAngle > -5 && steerAngle < 5)
+//            tv.setText("Steady");
+//          else if (steerAngle >= 5 && steerAngle < 60)
+//            tv.setText(String.format("Steer to Right: %f degrees", steerAngle - 90));
+//          else if (steerAngle <= -5 && steerAngle > -60)
+//            tv.setText(String.format("Steer to Left: %f degrees", steerAngle - 90));
+//          else
+//            tv.setText(String.format("Injuri bazi nakon"));
+//        }
+//      }, 0, GameConfig.REFRESH_INTERVAL);
+
       mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
       // If the adapter is null, then Bluetooth is not supported
       if (mBluetoothAdapter == null) {
@@ -78,158 +133,276 @@ public class MainActivity extends Activity {
         return;
       }
 
-    }
-    private void showDialog() {
-      AlertDialog.Builder builderSingle = new AlertDialog.Builder(MainActivity.this);
-      builderSingle.setTitle("Select a device: ");
+      initializeSensors();
 
-      builderSingle.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-          dialog.dismiss();
-        }
-      });
-
-      builderSingle.setAdapter(adapterPairedDevices, new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-          Object[] objects = pairedDevices.toArray();
-          BluetoothDevice device = (BluetoothDevice) objects[which];
-          mService.connect(device);
-          Toast.makeText(getApplicationContext(),"device choosen "+device.getName(),Toast.LENGTH_SHORT).show();
-          dialog.dismiss();
-        }
-      });
-      builderSingle.show();
     }
 
-    public void initializeDevices()
-    {
-      pairedDevices = mBluetoothAdapter.getBondedDevices();
+  private void showDialog() {
+    AlertDialog.Builder builderSingle = new AlertDialog.Builder(MainActivity.this);
+    builderSingle.setTitle("Select a device: ");
 
-      if (pairedDevices.size() > 0) {
-
-        for (BluetoothDevice device : pairedDevices) {
-          String deviceName = device.getName();
-          String deviceHardwareAddress = device.getAddress(); // MAC address
-
-          adapterPairedDevices.add(deviceName + "\n" + deviceHardwareAddress);
-        }
-      }
-    }
-
-    @Override
-    public void onStart() {
-      super.onStart();
-      mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-      // If BT is not on, request that it be enabled.
-      // initialize() will then be called during onActivityResult
-      if (!mBluetoothAdapter.isEnabled()) {
-        Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-        // Otherwise, setup the chat session
-      } else {
-        if (mService == null) initialize();
-        adapterPairedDevices = new ArrayAdapter(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item);
-        initializeDevices();
-      }
-    }
-    @Override
-    public synchronized void onResume() {
-      super.onResume();
-      // Performing this check in onResume() covers the case in which BT was
-      // not enabled during onStart(), so we were paused to enable it...
-      // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
-      if (mService != null) {
-        // Only if the state is STATE_NONE, do we know that we haven't started already
-        if (mService.getState() == BluetoothService.STATE_NONE) {
-          // Start the Bluetooth chat services
-          mService.start();
-        }
-      }
-    }
-    private void initialize() {
-      // Initialize the BluetoothService to perform bluetooth connections
-      mService = new BluetoothService(this, mHandler);
-      // Initialize the buffer for outgoing messages
-      mOutStringBuffer = new StringBuffer("");
-    }
-    @Override
-    public void onDestroy() {
-      super.onDestroy();
-      // Stop the Bluetooth chat services
-      if (mService != null) mService.stop();
-    }
-    private void ensureDiscoverable() {
-      if (mBluetoothAdapter.getScanMode() !=
-          BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
-        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-        startActivity(discoverableIntent);
-      }
-    }
-    /**
-     * Sends a message.
-     * @param message  A string of text to send.
-     */
-    private void sendMessage(String message) {
-      // Check that we're actually connected before trying anything
-      if (mService.getState() != BluetoothService.STATE_CONNECTED) {
-        Toast.makeText(this, "not connected", Toast.LENGTH_SHORT).show();
-        return;
-      }
-      // Check that there's actually something to send
-      if (message.length() > 0) {
-        // Get the message bytes and tell the BluetoothService to write
-        byte[] send = message.getBytes();
-        mService.write(send);
-        // Reset out string buffer to zero and clear the edit text field
-        mOutStringBuffer.setLength(0);
-      }
-    }
-    // The Handler that gets information back from the BluetoothService
-    @SuppressLint("HandlerLeak")
-    private final Handler mHandler = new Handler() {
+    builderSingle.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
       @Override
-      public void handleMessage(Message msg) {
-        switch (msg.what) {
-          case MESSAGE_STATE_CHANGE:
-            switch (msg.arg1) {
-              case BluetoothService.STATE_CONNECTED:
-                Toast.makeText(MainActivity.this, "connected to " + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
-                break;
-              case BluetoothService.STATE_CONNECTING:
-                Toast.makeText(MainActivity.this, "connecting", Toast.LENGTH_SHORT).show();
-                break;
-              case BluetoothService.STATE_LISTEN:
-              case BluetoothService.STATE_NONE:
-                Toast.makeText(MainActivity.this, "not connected", Toast.LENGTH_SHORT).show();
-                break;
-            }
-            break;
-          case MESSAGE_WRITE:
-            byte[] writeBuf = (byte[]) msg.obj;
-            // construct a string from the buffer
-            String writeMessage = new String(writeBuf);
-            break;
-          case MESSAGE_READ:
-            byte[] readBuf = (byte[]) msg.obj;
-            // construct a string from the valid bytes in the buffer
-            String readMessage = new String(readBuf, 0, msg.arg1);
-            Toast.makeText(MainActivity.this, readMessage, Toast.LENGTH_SHORT).show();
-            break;
-          case MESSAGE_DEVICE_NAME:
-            // save the connected device's name
-            mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
-            Toast.makeText(getApplicationContext(), "Connected to "
-                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
-            break;
-          case MESSAGE_TOAST:
-            Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
-                Toast.LENGTH_SHORT).show();
-            break;
-        }
+      public void onClick(DialogInterface dialog, int which) {
+        dialog.dismiss();
       }
+    });
+
+    builderSingle.setAdapter(adapterPairedDevices, new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        Object[] objects = pairedDevices.toArray();
+        BluetoothDevice device = (BluetoothDevice) objects[which];
+        mService.connect(device);
+        Toast.makeText(getApplicationContext(),"device choosen "+device.getName(),Toast.LENGTH_SHORT).show();
+        dialog.dismiss();
+      }
+    });
+    builderSingle.show();
+  }
+
+  public void initializeDevices()
+  {
+    pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+    if (pairedDevices.size() > 0) {
+
+      for (BluetoothDevice device : pairedDevices) {
+        String deviceName = device.getName();
+        String deviceHardwareAddress = device.getAddress(); // MAC address
+
+        adapterPairedDevices.add(deviceName + "\n" + deviceHardwareAddress);
+      }
+    }
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    // If BT is not on, request that it be enabled.
+    // initialize() will then be called during onActivityResult
+    if (!mBluetoothAdapter.isEnabled()) {
+      Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+      startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+      // Otherwise, setup the chat session
+    } else {
+      if (mService == null) initialize();
+      adapterPairedDevices = new ArrayAdapter(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item);
+      initializeDevices();
+    }
+  }
+  @Override
+  public synchronized void onResume() {
+    super.onResume();
+    // Performing this check in onResume() covers the case in which BT was
+    // not enabled during onStart(), so we were paused to enable it...
+    // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+    if (mService != null) {
+      // Only if the state is STATE_NONE, do we know that we haven't started already
+      if (mService.getState() == BluetoothService.STATE_NONE) {
+        // Start the Bluetooth chat services
+        mService.start();
+      }
+    }
+  }
+  private void initialize() {
+    // Initialize the BluetoothService to perform bluetooth connections
+    mService = new BluetoothService(this, mHandler);
+    // Initialize the buffer for outgoing messages
+    mOutStringBuffer = new StringBuffer("");
+  }
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    // Stop the Bluetooth chat services
+    if (mService != null) mService.stop();
+  }
+  private void ensureDiscoverable() {
+    if (mBluetoothAdapter.getScanMode() !=
+        BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+      Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+      discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+      startActivity(discoverableIntent);
+    }
+  }
+  /**
+   * Sends a message.
+   * @param message  A string of text to send.
+   */
+  private void sendMessage(String message) {
+    // Check that we're actually connected before trying anything
+    if (mService.getState() != BluetoothService.STATE_CONNECTED) {
+      Toast.makeText(this, "not connected", Toast.LENGTH_SHORT).show();
+      return;
+    }
+    // Check that there's actually something to send
+    if (message.length() > 0) {
+      // Get the message bytes and tell the BluetoothService to write
+      byte[] send = message.getBytes();
+      mService.write(send);
+      // Reset out string buffer to zero and clear the edit text field
+      mOutStringBuffer.setLength(0);
+    }
+  }
+  // The Handler that gets information back from the BluetoothService
+  @SuppressLint("HandlerLeak")
+  private final Handler mHandler = new Handler() {
+    @Override
+    public void handleMessage(Message msg) {
+      switch (msg.what) {
+        case MESSAGE_STATE_CHANGE:
+          switch (msg.arg1) {
+            case BluetoothService.STATE_CONNECTED:
+              Toast.makeText(MainActivity.this, "connected to " + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+              break;
+            case BluetoothService.STATE_CONNECTING:
+              Toast.makeText(MainActivity.this, "connecting", Toast.LENGTH_SHORT).show();
+              break;
+            case BluetoothService.STATE_LISTEN:
+            case BluetoothService.STATE_NONE:
+              Toast.makeText(MainActivity.this, "not connected", Toast.LENGTH_SHORT).show();
+              break;
+          }
+          break;
+        case MESSAGE_WRITE:
+          byte[] writeBuf = (byte[]) msg.obj;
+          // construct a string from the buffer
+          String writeMessage = new String(writeBuf);
+          break;
+        case MESSAGE_READ:
+          byte[] readBuf = (byte[]) msg.obj;
+          // construct a string from the valid bytes in the buffer
+          String readMessage = new String(readBuf, 0, msg.arg1);
+          Toast.makeText(MainActivity.this, readMessage, Toast.LENGTH_SHORT).show();
+          break;
+        case MESSAGE_DEVICE_NAME:
+          // save the connected device's name
+          mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+          Toast.makeText(getApplicationContext(), "Connected to "
+              + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+          break;
+        case MESSAGE_TOAST:
+          Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+              Toast.LENGTH_SHORT).show();
+          break;
+      }
+    }
+  };
+
+private void initializeSensors() {
+    sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+    accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    magnetometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+    gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+    gameRotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+
+    sensorManager.registerListener(accelerometerListener, accelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    sensorManager.registerListener(magnetometerListener, magnetometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    sensorManager.registerListener(gyroscopeListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    sensorManager.registerListener(gravityListener, gravitySensor, SensorManager.SENSOR_DELAY_FASTEST);
+    sensorManager.registerListener(gameRotationListener, gameRotationSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+private SensorEventListener accelerometerListener = new SensorEventListener() {
+@Override
+public void onSensorChanged(SensorEvent sensorEvent) {
+    double deltaT = (sensorEvent.timestamp - accLastEventTimestamp) / 1e9;
+    if (deltaT > 0.1) {
+    accelerometerTheta = new _3dVector(sensorEvent.values[0],
+    sensorEvent.values[1],
+    sensorEvent.values[2]);
+    accLastEventTimestamp = sensorEvent.timestamp;
+
+    }
+
+    }
+
+@Override
+public void onAccuracyChanged(Sensor sensor, int i) {
+    }
     };
 
-  }
+private SensorEventListener magnetometerListener = new SensorEventListener() {
+@Override
+public void onSensorChanged(SensorEvent sensorEvent) {
+    double deltaT = (sensorEvent.timestamp - magLastEventTimestamp) / 1e9;
+
+    if (deltaT > 0.1) {
+    magnetometerTheta = new _3dVector(sensorEvent.values[0],
+    sensorEvent.values[1],
+    sensorEvent.values[2]);
+    magLastEventTimestamp = sensorEvent.timestamp;
+
+    }
+    }
+
+@Override
+public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+    };
+
+
+private SensorEventListener gyroscopeListener = new SensorEventListener() {
+@Override
+public void onSensorChanged(SensorEvent sensorEvent) {
+    double deltaT = (sensorEvent.timestamp - gyroscopeLastEventTimestamp) / 1e9;
+
+    if (deltaT > 0.1) {
+    gyroscopeTheta = new _3dVector(sensorEvent.values[0],
+    sensorEvent.values[1],
+    sensorEvent.values[2]);
+    gyroscopeLastEventTimestamp = sensorEvent.timestamp;
+
+    }
+    }
+
+@Override
+public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+    };
+
+
+private SensorEventListener gravityListener = new SensorEventListener() {
+@Override
+public void onSensorChanged(SensorEvent sensorEvent) {
+    double deltaT = (sensorEvent.timestamp - gravityLastEventTimestamp) / 1e9;
+
+    if (deltaT > 0.1) {
+    gravityTheta = new _3dVector(sensorEvent.values[0],
+    sensorEvent.values[1],
+    sensorEvent.values[2]);
+    gravityLastEventTimestamp = sensorEvent.timestamp;
+
+    }
+    }
+
+@Override
+public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+    };
+
+
+private SensorEventListener gameRotationListener = new SensorEventListener() {
+@Override
+public void onSensorChanged(SensorEvent sensorEvent) {
+    double deltaT = (sensorEvent.timestamp - gameRotationLastEventTimestamp) / 1e9;
+
+    if (deltaT > 0.1) {
+    gameRotationTheta = new _3dVector(sensorEvent.values[0],
+    sensorEvent.values[1],
+    sensorEvent.values[2],
+    sensorEvent.values[3]);
+    gameRotationLastEventTimestamp = sensorEvent.timestamp;
+
+    }
+    }
+
+@Override
+public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+    };
+
+
+    }
---
+++ CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/core/_3dVector.java
@@ -0,0 +1,27 @@
+package com.mobina.cocoruncontroller.core;
+
+public class _3dVector {
+  public Double x, y, z;
+
+  public _3dVector(double x, double y, double z, double scaling_factor) {
+    this.x = x * scaling_factor;
+    this.y = y * scaling_factor;
+    this.z = z * scaling_factor;
+  }
+
+  public _3dVector(double x, double y, double z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  public void vectorAddition(_3dVector newVec) {
+    this.x += newVec.x;
+    this.y += newVec.y;
+    this.z += newVec.z;
+  }
+
+  public _3dVector multiplyVectorByNum(double number) {
+    return new _3dVector(this.x * number, this.y * number, this.z * number);
+  }
+}
