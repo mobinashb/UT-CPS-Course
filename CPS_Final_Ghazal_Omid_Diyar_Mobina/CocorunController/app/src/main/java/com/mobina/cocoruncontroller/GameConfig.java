aec6fdfdf32e19051d10f23b9ba6fd5ced45c5d7
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2021-05-31 17:14:55 +0430

    Fix bug

--- CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/GameConfig.java
+++ CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/GameConfig.java
@@ -1,5 +1,7 @@
 package com.mobina.cocoruncontroller;
 
 public class GameConfig {
-  public enum COMMAND {L, R}
+  public enum COMMAND {L, R};
+  public static final int REFRESH_RATE = 1;
+
 }
--- CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/SensorActivity.java
+++ CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/SensorActivity.java
@@ -20,37 +20,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.lang.Math;
 
-class _3dVector {
-    public Double x, y, z;
-
-    public _3dVector(double x, double y, double z, double scaling_factor) {
-        this.x = x * scaling_factor;
-        this.y = y * scaling_factor;
-        this.z = z * scaling_factor;
-    }
-
-    public _3dVector(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public void vectorAddition(_3dVector newVec) {
-        this.x += newVec.x;
-        this.y += newVec.y;
-        this.z += newVec.z;
-    }
-
-    public _3dVector multiplyVectorByNum(double number) {
-        return new _3dVector(this.x*number, this.y*number, this.z*number);
-    }
-}
-
-
-class GameConfig {
-    public static final int REFRESH_RATE = 1;
-}
-
+import com.mobina.cocoruncontroller.GameConfig;
 
 
 public class SensorActivity extends AppCompatActivity {
