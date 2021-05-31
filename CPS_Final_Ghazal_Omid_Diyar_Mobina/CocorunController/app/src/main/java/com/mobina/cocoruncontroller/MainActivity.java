8ce874e6dbec7ae6d673e7c9aef8326992417ff7
Mobina <shbmobina@gmail.com>
2021-05-31 14:07:14 +0430

    Initialize controller layout

--- CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/MainActivity.java
+++ CPS_Final_Ghazal_Omid_Diyar_Mobina/CocorunController/app/src/main/java/com/mobina/cocoruncontroller/MainActivity.java
@@ -2,12 +2,20 @@ package com.mobina.cocoruncontroller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    // Set fullscreen
+    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    // Set No Title
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.activity_main);
   }
 }
